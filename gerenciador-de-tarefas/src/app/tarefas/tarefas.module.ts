ed4f0bf24ed20bf9c298c9959657d92821c334f6
bueno <joaopedrobueno2910@gmail.com>
2023-07-26 12:22:57 -0300

    Finalizando projeto Gerenciador de Tarefas
    Finalizando projeto Gerenciador de Tarefas

--- gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -6,12 +6,14 @@ import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CadastrarTarefaComponent } from './cadastrar/cadastrar-tarefa.component';
 import { EditarTarefaComponent } from './editar/editar-tarefa.component';
+import { TarefaConcluidaDirective } from './shared/tarefa-concluida.directive';
 
 @NgModule({
   declarations: [
     ListarTarefasComponent,
     CadastrarTarefaComponent,
-    EditarTarefaComponent
+    EditarTarefaComponent,
+    TarefaConcluidaDirective
   ],
   imports: [
     CommonModule,
