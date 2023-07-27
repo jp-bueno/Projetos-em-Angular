18520e546cecb1acbe4044cea5706b7a190d3006
bueno <joaopedrobueno2910@gmail.com>
2023-07-27 15:50:32 -0300

    finalizando projeto angular
    finalizando projeto angular

--- projeto-final/src/app/app-routing.module.ts
+++ projeto-final/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardRoutes } from './dashboard/dashboard-routing.module';
+import { CalculadoraRoutes } from './calculadora/calculadora-routing.module';
+import { ConversorRoutes } from './conversor/conversor-routing.module';
+import { TarefaRoutes } from './tarefas';
+import { JogoDaVelhaRoutes } from './jogo-da-velha/jogo-da-velha-routing.module';
 
-const routes: Routes = [];
+export const routes: Routes = [
+	{ 
+		path: '', 
+		redirectTo: '/dashboard', 
+		pathMatch: 'full' 
+	},
+	...DashboardRoutes,
+	...CalculadoraRoutes,
+  ...ConversorRoutes,
+  ...TarefaRoutes,
+  ...JogoDaVelhaRoutes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projeto-final/src/app/app.module.ts
+++ projeto-final/src/app/app.module.ts
@@ -1,8 +1,15 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { DashboardModule } from './dashboard';
+import { CalculadoraModule } from './calculadora';
+import { ConversorModule } from './conversor';
+import { TarefasModule } from './tarefas';
+import { JogoDaVelhaModule } from './jogo-da-velha';
 
 @NgModule({
   declarations: [
@@ -10,6 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    DashboardModule,
+    CalculadoraModule,
+    ConversorModule,
+    TarefasModule,
+    JogoDaVelhaModule,
     AppRoutingModule
   ],
   providers: [],
